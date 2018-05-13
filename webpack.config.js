6f831dfba62ec85b0d295860a2408dfef12108ca
LilyReni <irisren7799@gmail.com>
2018-05-13 13:16:37 -0700

    webpack set

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+var path = require('path');
+
+//bundle file output
+var BUILD_DIR = path.resolve(__dirname, 'public');
+
+//application's codebase
+var APP_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+	entry: APP_DIR + '/components/index.jsx',
+	output: {
+		path: BUILD_DIR,
+		filename: 'bundle.js'
+	},
+	module : {
+		rules: [
+		{
+			test: /\.js(x)$/,
+			exclude: /(node_modules|bower_components)/,
+			use: {
+				loader: 'babel-loader',
+				//!!!leave it blank for now
+				options: {
+
+				}
+			}
+		}
+		]
+	}
+}; 
+
+module.exports = config;
